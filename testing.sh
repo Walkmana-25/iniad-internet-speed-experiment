1b57ecb9c301d0f21ae33068cc796ad3921dee1a
Yuta Takahashi <y.highbridge04@gmail.com>
2024-10-04 07:39:50 +0000

    Refactor testing script to include specific speedtest metrics in output

--- testing.sh
+++ testing.sh
@@ -2,5 +2,5 @@
 for i in `seq 3`
 do
     date >> /home/ubuntu/output.txt
-    speedtest -s 14623 --accept-license >> /home/ubuntu/output.txt
+    speedtest -s 14623 --accept-license | grep -e 'Idle Latency' -e 'Download' -e 'Upload' >> /home/ubuntu/output.txt
 done
